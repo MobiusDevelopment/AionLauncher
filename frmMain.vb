16562488f1d11b2369b424306b5483d4c1f3313c
Mobius Dev <mobius@cyber-wizard.com>
2017-09-02 20:10:34 +0300

    Initial commit.

---
+++ frmMain.vb
@@ -0,0 +1,28 @@
+﻿Imports System.IO
+
+Public Class frmMain
+    Dim INI_FILE As String = "AionLauncher.ini"
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'make form invisible
+        Me.Visible = False
+
+        'get info from file, if it exists
+        Dim shellCommand = ""
+        If File.Exists(INI_FILE) Then
+            Using fileReader As StreamReader = New StreamReader(INI_FILE)
+                shellCommand = fileReader.ReadLine
+            End Using
+        End If
+
+        'execute
+        Try
+            Shell(shellCommand, vbNormalFocus)
+        Catch ex As Exception
+            MsgBox("File not found! Try checking your files.", MsgBoxStyle.Critical)
+        End Try
+
+        'close
+        End
+    End Sub
+End Class
